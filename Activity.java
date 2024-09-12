17126ae059d065275732c3492b9ce963b8ff6b4e
ArizalaRonAndrew <ronandrewarizala@gmail.com>
2024-09-12 15:12:32 +0800

    initial commit

---
+++ Activity.java
@@ -0,0 +1,13 @@
+public class Activity{
+    public static void main(String[] args){
+        Dog aso = new Dog();
+
+        aso.Puppies[0] = "bah";
+        aso.Puppies[1] = "beh";
+        aso.Puppies[2] = "bih";
+        aso.Puppies[3] = "boh";
+        aso.Puppies[4] = "buh";
+
+        System.out.println(aso.Dog[2]);
+    }
+}
---
+++ Dog.java
@@ -0,0 +1,10 @@
+public class Dog {
+    String Name;
+    boolean isMale;
+    int numPup;
+    Dog[] Puppies = new Dog[5];
+
+ public Dog doglist(){
+    return Puppies;
+ }
+ }
