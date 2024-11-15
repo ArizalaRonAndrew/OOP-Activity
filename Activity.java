1e774d00670a8bbce891901520df8e74f26e5e8c
ArizalaRonAndrew <ronandrewarizala18@gmail.com>
2024-11-15 14:29:51 +0800

    Update Activity.java

--- Activity.java
+++ Activity.java
@@ -1,18 +1,22 @@
-public class Activity{
+public class Main {
     public static void main(String[] args) {
-        Dog aso = new Dog(5);
-
-      
         
-        aso.aww("Bonatan");
-        System.out.println(aso.ngalan());
-        aso.bark();
-        System.out.println(aso.bilang(5));
-        aso.setNum();
-        String[] puppyNames = {"Buddy", "Bella","ron", "Charlie"};
-        aso.setDoggyNames(puppyNames);
-        aso.listPuppies();
-       
+        Student StudentInstance = new Student("charlie", "san juan Balayan", "BSIT", 2);
+        System.out.println("Student Details:");
+        System.out.println("Student Name: " + StudentInstance.getName());
+        System.out.println("Student Address: " + StudentInstance.getAddress());
+        System.out.println("Student Program: " + StudentInstance.getProgram());
+        System.out.println("Student Year: " + StudentInstance.getYear());
 
-    }
-}
+        System.out.println(StudentInstance.toString());
+
+        
+        Staff StaffInstance = new Staff("Ron", "Calantas Calaca City", "english Department", 40000.0);
+        System.out.println("\nStaff Details:");
+        System.out.println("Staff Name: " + StaffInstance.getName());
+        System.out.println("Staff Address: " + StaffInstance.getAddress());
+        System.out.println("Staff School: " + StaffInstance.getSchool());
+        System.out.println("Staff Salary: " + StaffInstance.getSalary());
+
+        System.out.println(StaffInstance.toString());
+    }}
