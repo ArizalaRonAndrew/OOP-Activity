d8faffbf17ae23da9ed6f7e274eccfbf1af16526
ArizalaRonAndrew <ronandrewarizala18@gmail.com>
2024-11-15 09:15:34 +0800

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,26 @@
+public static class Student extends Person {
+        private String Program;
+        private int Year;
+
+        public Student(String Name, String Address, String Program, int Year) {
+            super(Name, Address);
+            this.Program = Program;
+            this.Year = Year;
+        }
+
+        public void setProgram(String Program) {
+            this.Program = Program;
+        }
+
+        public String getProgram() {
+            return Program;
+        }
+
+        public void setYear(int Year) {
+            this.Year = Year;
+        }
+
+        public int getYear() {
+            return Year;
+        }
+    }
